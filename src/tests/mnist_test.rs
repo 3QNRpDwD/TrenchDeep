8451803d4d8da47f81e9871b5f9a814e060c71b5
3QNRpDwD <120706785+3QNRpDwD@users.noreply.github.com>
2026-09-08 22:54:17 +0900

    체크포인트를 비활성화 할것
    너 스 레 떨 지 말 것

--- src/tests/mnist_test.rs
+++ src/tests/mnist_test.rs
@@ -52,7 +52,7 @@ fn mlp_mnist_classification_integration_test() -> MlResult<()> {
 
     if accuracy > config.required_accuracy {
         info!("Target accuracy achieved! ({:.2}% > {:.2}%)", accuracy, config.required_accuracy);
-        crate::trainer::CheckpointableModel::save_checkpoint(&mlp, std::path::Path::new(&config.model_save_path))?;
+        # crate::trainer::CheckpointableModel::save_checkpoint(&mlp, std::path::Path::new(&config.model_save_path))?;
     } else {
         warn!("Target accuracy NOT met. (Actual: {:.2}%, Required: {:.2}%).", accuracy, config.required_accuracy);
     }
